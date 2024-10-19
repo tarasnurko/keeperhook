fa54c0fa369f59133e43a90954fb683f91f8ffa2
Taras Nurko <tarasnurko84@gmail.com>
2024-10-19 15:45:44 +0300

    feat: finished adding main functionality

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-SIGMA SIGMA ON THE WALL, WHO'S THE SKIBIDIEST OF THEM ALL?
+# KeeperHook
+
+It's uniswap v4 based hook that allow users exchange their transaction work in return of lower swap fees and additional collected fee from appropriate liquidity position in current pool.
+
+## How does it work?
+
+## Why?
+
+Let's imagine situation that you are small protocol with little money that try to build code in the shortest period of time. Your smart-contract includes simple AutomationCompatible interface...
+
+1. Why do you need AutomationCompatible interface? This interface is usually used when smart-contract need to update some data dependint on other state-changing stuff onchain ([https://docs.chain.link/chainlink-automation](read more from chainlink)). This can be used in various ocasions, for example:
+  - automate liquidations of unhealthy users
+  - update contract state that should be updated when other contract state (external from your system) is also updated  
+  - when function need to be called consistently with some interval
+  - and it's small part of what Automation can do to improve protocol...
+
+So... Why not just use Chainlink or create own script to call smart-contract consistently??? Of courese you can, but...:
+  - Own code. Firstly you need to create script that would check state as you want, then you need to somwhere deploy this script, you need to make sure this script never fails, you use RPC calls that can be very limited, you need to rent some server to host this script and more and more time and material expences.
+  - Chainlink. Chainlink solves problem described above but it still need to be managed and more important, YOU STILL NEED TO PAY FOR TRANSACTION!!! (bet we dont😊)
+
+Instead **KeeperHook** add ability to protocol owners to provide liquidity which is preserved and does not disappear anywhere. And that's a pros for user as user make transaction, checks whether you contract need upkeep and perform upkeep if it's needed. This gives advantages to both the user and protocol owner. WIN-WIN SITUATION.
--- src/KeeperHook.sol
+++ src/KeeperHook.sol
@@ -14,17 +14,29 @@ import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeS
 import {Actions} from "v4-periphery/src/libraries/Actions.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {Time} from "@openzeppelin/contracts/utils/types/Time.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract KeeperHook is BaseHook {
+contract KeeperHook is BaseHook, AccessControl {
     // @dev uniswap-v4 position manager
     IPositionManager public immutable positionManager;
+    address public slasher;
 
     // FEES
     uint24 public constant ZERO_ITERATIONS_FEE = 500; // 0.05%, 1000000 = 100%
     uint24 public constant DEFAULT_FEE = 100; // 0.01%
 
+    // uint256 public constant KEEPERS_ORDER_MAX_LENGTH = 10;
+
+    // ROLES
+    bytes32 constant SLAHSER_ROLE = keccak256("SLAHSER_ROLE");
+    bytes32 constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
     // GAS
+    // @dev max total gas that can be spent by user while checkUpkeep + performUpkeep for all keepers in beforeSwap, must be less then block gas
     uint128 public immutable maxTotalGas;
+
+    uint48 public constant MODIFY_POSITION_LIQUIDITY_COOLDOWN = 4 hours;
+    uint48 public constant POSITION_WITHDRAWAL_COOLDOWN = 1 days;
 
     // @dev struct that represetns parameters when calling AutomationCompatible functions
     struct KeepersData {
@@ -33,7 +45,29 @@ contract KeeperHook is BaseHook {
         address contractAddress;
     }
 
+    /**
+     * @param date after which withdrawal request can be completed
+     */
+    struct LiquidityWithdrawalRequest {
+        bool completed;
+        uint48 date;
+        address liquidityOwner;
+        PoolId poolId;
+        uint256 positionTokenId;
+    }
+
+    error SenderIsNotLiquidityOwner();
+    error PositionTokenIsNotReturned();
+    error LiquidityAfterLessThenBefore();
     error LiquidityPositionForPoolIsNotEmpty();
+    error TotalGasExceedsMaxTotalGas();
+    error LiquidityOwnerIsAlreadySlashed();
+
+    /**
+     * @notice event when checkUpkeep returns true but performUpkeep failed
+     * @dev such performUpkeep can be malicious and be subsequet to slashing
+     */
+    event PerformUpkeepFailed(address liquidityOwner, PoolId poolId, address contractAddress);
 
     // @dev owner of deposited liquidity position token
     mapping(uint256 positionTokenId => address liquidityOwner) public liquidityPositionOwners;
@@ -42,18 +76,31 @@ contract KeeperHook is BaseHook {
     // @dev represent contracts that need to be upkeeped
     mapping(PoolId poolId => address[] liquidityOwners) public keeperOrders;
 
-    constructor(address _poolManager, address _positionManager, uint128 _maxTotalGas)
+    // Liquidity manipulation cooldown
+    mapping(uint256 positionTokenId => uint48 expiresAt) public modifyLiquidityPositionCooldowns;
+    // Liquidity withdrawal requests
+    mapping(bytes32 withdrawalHash => LiquidityWithdrawalRequest request) public liquidityWithdrawalRequests;
+
+    // Slash info mappings
+    mapping(address liquidityOwner => bool isSlashed) public slashedLiquidityOwner;
+    mapping(address keepersContractAddress => bool isSlashed) public slashedKeepersContracts;
+
+    constructor(address _poolManager, address _positionManager, uint128 _maxTotalGas, address _slasher)
         BaseHook(IPoolManager(_poolManager))
     {
         validateHookAddress(this);
         positionManager = IPositionManager(_positionManager);
         maxTotalGas = _maxTotalGas;
+        slasher = _slasher;
     }
 
     /////////////////////////////////////
     //          HOOK FUNCTIONS         //
     /////////////////////////////////////
 
+    /**
+     * @notice main functionality of the protocol. If user successfuly performs upkeep they pay 0 swap fee and also get accumulate fees of appropriate poolid liquidity position
+     */
     function beforeSwap(
         address sender,
         PoolKey calldata poolKey,
@@ -74,7 +121,7 @@ contract KeeperHook is BaseHook {
             liquidityOwner = keeperOrders[poolId][i];
             KeepersData memory keepersData = keepers[liquidityOwner];
 
-            // TODO: write function to check for data without changing storage
+            // TODO: write function to check for data without changing storage (use revertDelegateCall?)
             (bool success, bytes memory data) = keepersData.contractAddress.call{gas: keepersData.maxCheckUpkeepGas}(
                 abi.encodeWithSelector(AutomationCompatibleInterface.checkUpkeep.selector, bytes(""))
             );
@@ -93,6 +140,8 @@ contract KeeperHook is BaseHook {
                 hasUpkeepPerformed = true;
                 break;
             }
+
+            emit PerformUpkeepFailed(liquidityOwner, poolId, keepersData.contractAddress);
         }
 
         if (!hasUpkeepPerformed) {
@@ -127,7 +176,10 @@ contract KeeperHook is BaseHook {
     //        LIQUIDITY FUNCTIONS      //
     /////////////////////////////////////
 
-    function depositLiquidity(uint256 positionTokenId) external {
+    // function that allows liquidity owner to change liquidityPosition to new (for example for different amount of liquidity or for different ticks)
+    // function changeLiquidityPosition(uint256 newTokenId) external {}
+
+    function depositLiquidityPosition(uint256 positionTokenId) external {
         address liquidityOwner = msg.sender;
         (PoolKey memory poolKey,) = positionManager.getPoolAndPositionInfo(positionTokenId);
         PoolId poolId = poolKey.toId();
@@ -137,7 +189,7 @@ contract KeeperHook is BaseHook {
         _positionManagerAsERC721().transferFrom(liquidityOwner, address(this), positionTokenId);
 
         liquidityPositionOwners[positionTokenId] = liquidityOwner;
-        // modifyLiquidityPositionCooldowns[positionTokenId] = Time.timestamp() + MODIFY_POSITION_LIQUIDITY_COOLDOWN;
+        modifyLiquidityPositionCooldowns[positionTokenId] = Time.timestamp() + MODIFY_POSITION_LIQUIDITY_COOLDOWN;
         liquidityPositions[liquidityOwner][poolId] = positionTokenId;
         keeperOrders[poolId].push(liquidityOwner);
 
@@ -145,9 +197,113 @@ contract KeeperHook is BaseHook {
         _checkOrderOperationsInGasLimit(poolId);
     }
 
-    function withdrawLiquidity() external {}
+    /**
+     * @dev when liquidity owner is being liquidates so they dont front-run and not withdraw all liquidity before slashing as there is cooldown between withdrawal request and withdrawal completion
+     */
+    function withdrawLiquidityPosition(PoolId poolId, uint256 positionTokenId)
+        external
+        returns (LiquidityWithdrawalRequest memory, bytes32)
+    {
+        address liquidityOwner = msg.sender;
 
-    // collect accumulated fees from position
+        require(positionTokenId != 0);
+        require(liquidityPositionOwners[positionTokenId] == liquidityOwner);
+        require(liquidityPositions[liquidityOwner][poolId] == positionTokenId);
+
+        delete liquidityPositionOwners[positionTokenId];
+        // remove from keeperOrders
+        uint256 orderLength = keeperOrders[poolId].length;
+        for (uint256 i; i < orderLength; i++) {
+            if (keeperOrders[poolId][i] == liquidityOwner) {
+                keeperOrders[poolId][i] = keeperOrders[poolId][orderLength - 1];
+                keeperOrders[poolId].pop();
+                break;
+            }
+        }
+
+        LiquidityWithdrawalRequest memory liquidityWithdrawalRequest;
+        liquidityWithdrawalRequest.date = Time.timestamp() + POSITION_WITHDRAWAL_COOLDOWN;
+        liquidityWithdrawalRequest.liquidityOwner = liquidityOwner;
+        liquidityWithdrawalRequest.poolId = poolId;
+        liquidityWithdrawalRequest.positionTokenId = positionTokenId;
+
+        bytes32 hash = _getLiquidityWithdrawalRequestHash(liquidityWithdrawalRequest);
+
+        return (liquidityWithdrawalRequest, hash);
+    }
+
+    function completeWithdrawLiquidityPosition(bytes32 requestHash) external {
+        LiquidityWithdrawalRequest storage request = liquidityWithdrawalRequests[requestHash];
+
+        require(request.date != 0, "Request doesnt exist");
+        require(Time.timestamp() > request.date);
+        require(!request.completed, "Requst has been already completed");
+
+        request.completed = true;
+
+        _positionManagerAsERC721().safeTransferFrom(address(this), request.liquidityOwner, request.positionTokenId);
+    }
+
+    /**
+     * @notice slashes malicious liquidity owner
+     * @dev sends all slashed positionTokens to slasher
+     * @dev executes in case keepers contract make some malicious actions like
+     * - doesnt allow any user to performUpkeep by always reverting
+     * - returngasbombs user, blocking functionality
+     * - other malicious actions that dont allow user to correctly check upkeep or perform upkeep
+     * @param liquidityOwner liquidity owner address to be liquidated
+     * @param poolIds array of poolIds where liquidityOwner has any liquidity
+     * @param positionTokenIds array of owned positionTokenIds
+     */
+    function slashKeepers(address liquidityOwner, PoolId[] calldata poolIds, uint256[] calldata positionTokenIds)
+        external
+        onlyRole(SLAHSER_ROLE)
+    {
+        require(!slashedLiquidityOwner[liquidityOwner]);
+        require(!slashedKeepersContracts[liquidityOwner]);
+
+        slashedLiquidityOwner[liquidityOwner] = true;
+        slashedKeepersContracts[liquidityOwner] = true;
+
+        delete keepers[liquidityOwner];
+
+        uint256 poolIdsLength = poolIds.length;
+        uint256 positionTokenIdsLength = positionTokenIds.length;
+
+        // delete all pool liquidityPositions
+        for (uint256 i = 0; i < poolIdsLength; ++i) {
+            PoolId poolId = poolIds[i];
+
+            require(liquidityPositions[liquidityOwner][poolId] != 0);
+            delete liquidityPositions[liquidityOwner][poolId];
+
+            address[] memory poolLiquidityOwners = keeperOrders[poolId];
+            uint256 len = poolLiquidityOwners.length;
+
+            // remove liquidityPosition
+            for (uint256 j = 0; j < len; ++j) {
+                if (poolLiquidityOwners[j] == liquidityOwner) {
+                    poolLiquidityOwners[j] = poolLiquidityOwners[len - 1];
+                    break;
+                }
+            }
+
+            keeperOrders[poolId] = poolLiquidityOwners;
+            keeperOrders[poolId].pop();
+
+            _sortKeeperOrders(poolId);
+        }
+
+        // delete liquidityOwner from onwing liquidityPosition
+        for (uint256 i = 0; i < positionTokenIdsLength; ++i) {
+            uint256 positionTokenId = positionTokenIds[i];
+
+            require(liquidityPositionOwners[positionTokenId] != address(0));
+            delete liquidityPositionOwners[positionTokenId];
+        }
+    }
+
+    // @notice collect accumulated fees from position
     function _withdrawFees(PoolKey calldata poolKey, address to, uint256 positionTokenId) internal {
         // https://docs.uniswap.org/contracts/v4/quickstart/manage-liquidity/collect
 
@@ -160,6 +316,47 @@ contract KeeperHook is BaseHook {
 
         uint256 deadline = block.timestamp + 60;
         positionManager.modifyLiquidities(abi.encode(actions, params), deadline);
+    }
+
+    function _getLiquidityWithdrawalRequestHash(LiquidityWithdrawalRequest memory req)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encode(req.date, req.liquidityOwner, req.poolId, req.positionTokenId));
+    }
+
+    // function _checkLiquidityPosition
+
+    /**
+     * @notice updates liquidityPosition without removing it from contract
+     * @notice can be used when protocol want to add liquidity or/and change tick lower/upper to    (concentrate liquidity) generate more fees (for example)
+     */
+    function updateLiquidityPosition(uint256 positionTokenId) external {
+        if (msg.sender != liquidityPositionOwners[positionTokenId]) {
+            revert SenderIsNotLiquidityOwner();
+        }
+
+        // (PoolKey memory poolKey, ) = positionManager.getPoolAndPositionInfo(positionTokenId);
+        // PoolId poolId = poolKey.toId();
+
+        uint128 liquidityBefore = positionManager.getPositionLiquidity(positionTokenId);
+
+        _positionManagerAsERC721().safeTransferFrom(address(this), msg.sender, positionTokenId);
+
+        if (_positionManagerAsERC721().ownerOf(positionTokenId) != address(this)) {
+            revert PositionTokenIsNotReturned();
+        }
+
+        uint128 liquidityAfter = positionManager.getPositionLiquidity(positionTokenId);
+
+        if (liquidityAfter < liquidityBefore) {
+            revert LiquidityAfterLessThenBefore();
+        }
+    }
+
+    function setKeepersData(KeepersData calldata keepersData) external {
+        keepers[msg.sender] = keepersData;
     }
 
     /////////////////////////////////////
